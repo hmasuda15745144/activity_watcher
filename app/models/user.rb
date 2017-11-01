d44b796bcfc69bb274c156fb0dbf2820fb73607d
hal13 <a15z8my@aiit.ac.jp>
2017-11-01 04:28:33 +0000

    add registration page but test not yet

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,25 @@ class User < ApplicationRecord
   
   include ActiveRecord::Confirmable
   
+  VALID_EMAIL_DEFAULT = /\A\S+@\S+\.\S+\z/
+  validates :email,
+    presence: true,
+    uniqueness: true,
+    length: { maximum: 128 },
+    format: { with: VALID_EMAIL_DEFAULT, message: "は正しいアドレスではありません" }
+    
+  validates :user_full_name,
+    presence: true,
+    length: { maximum: 64 }
+    
+  validates :slack_user, 
+    presence: true,
+    length: { maximum: 64 }
+    
+  validates :student_no,
+    presence: true,
+    length: { maximum: 64 }
+  
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth['provider']
