5dd05503cf61a1ef3dacf48bb5f5351c71093afb
QILIGERU <a1532gq@aiit.ac.jp>
2017-11-15 21:26:04 +0900

    taskテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20171112062145) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "teams", force: :cascade do |t|
     t.string   "name"
     t.string   "source"
